4dfaccf36d58a5e13bee17bf1fc8b79f33a5d5b8
Taiwo Joseph <60525931+taiwojay@users.noreply.github.com>
2021-07-25 20:09:26 +0100

    Update AboutUs.js

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -7,14 +7,14 @@ export default function AboutUs() {
         <div className='page '>
           <div className='essay fade-in'>
           <h3>About Us</h3>
-          <p> Over the years, many small and medium scale enterprises are faced with financial instability which as a result has reduced their businesses from zero to nothing.</p>
+          <p> Over the years, many small and medium scale enterprises have been faced with financial instability which has reduced their businesses from zero to nothing.</p>
         
-          <p> Therefore, to catapult low and medium income earners to the atmosphere of financial stability is why Jeseku was created. </p>
+          <p> Therefore, to catapult low and medium income earners to the atmosphere of financial stability, Jeseku was created. </p>
      
-          <p>Jeseku is a registered digital limited liability and multi-purpose cooperative network established in 2020, borne out of the quest to expand start-up businesses and render financial assistance to entrepreneurs by creating smooth access to loan facilities.</p>
+          <p>Jeseku is a registered digital limited liability and multi-purpose cooperative network established in 2020, born out of the quest to expand start-up businesses and render financial assistance to entrepreneurs by creating smooth access to loan facilities.</p>
     
           <p>At Jeseku, we believe that SME owners can achieve their entrepreneurial dreams with collaborative efforts. Thus, we are committed to be an intermediary between their abilities and their future.</p>
-          <p> Inorder to reach out to a wide range of entrepreneurs, we devise financial expansion mechanism using the digital space to offer services such as; deposit and savings channels, advance grants, and loaning with little or no interest rates, depending on the business model.</p>
+          <p> In order to reach out to a wide range of entrepreneurs, we devise financial expansion mechanism using the digital space to offer services such as; deposit and savings channels, advance grants, and loaning with little or no interest rates, depending on the business model.</p>
           <p>Nigeria can have a sustainable cash flow if young professionals can lend, save and pay for their essential commodities; that is why Jeseku is at your doorstep to promote financial stability. </p>
           <h3>Vision</h3>
            To promote financial stability in the country and become the highest rated lending network both at the local level and the international spheres.
